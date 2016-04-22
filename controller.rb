599874777548a7bc0bd1f56ade2fc50a5c24301a
DMelon113 <dmelonid@lsoc.org>
2016-04-22 10:28:38 -0500

    more

--- controller.rb
+++ controller.rb
@@ -7,3 +7,15 @@ end
 
 post '/guess' do
 	if params[:trip] == "Miami"
+		erb :miami
+	elsif params[:trip] == "Wisconsin"
+
+		end
+	end
+post '/guess/miami' do
+			if params[:activity] == "Scuba"
+			erb :scuba
+		end
+	end
+
+
