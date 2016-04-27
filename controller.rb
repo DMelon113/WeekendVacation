d378ee89754fcc207e2ddd2a75526151285f3d38
DMelon113 <dmelonid@lsoc.org>
2016-04-27 15:01:54 -0500

    more

--- controller.rb
+++ controller.rb
@@ -12,20 +12,41 @@ get '/decision/:choice' do
 	case params[:choice]
 	when "Miami"
 	@title = "Weekend Vacation"
-	@choice_1 = "Scuba Dive"
-	@choice_2 = "Go on a Boat"
+	@choice_1 = "Scuba_Dive"
+	@choice_2 = "Go_on_a_Boat"
 	erb :index
+
+when "Go_on_a_Boat"
+	@title = "Weekend Vacation"
+	@story = "You make yourself a cocktail enjoy the scenery. You eventually get bored and start sailing back to shore. A police boat sees you and thinks you are weaving. He makes you take a breathalizer test and you spend the night in prison for a D.U.I "
+
+	when "Scuba_Dive"
+	@title = "Weekend Vacation"
+	@choice_1 = "Partially_fill_up_tank"
+	@choice_2 = "Fully_fill_up_tank"
+	erb :index
+
+when "Partially_fill_up_tank"
+	@title = "Weekend Vacation"
+	@story = "You go for a brief but cool dive. You resurface with no issues and enjoy the rest of your weekend."
+
+when "Fully_fill_up_tank"
+	@title = "Weekend Vacation"
+	@story = "Wow, you are underwater for a pretty long time. Suddenly, you see a black tip shark which turns and attacks you. R.I.P"
+
 
 	when "Wisconsin"
 		@title = "Weekend Vacation"	
-		@choice_1 = "Go on a Jet Ski"
-		@choice_2 = "Go Fishing on a Boat"
+		@choice_1 = "Go_on_a_Jet_Ski"
+		@choice_2 = "Go_Fishing_on_a_Boat"
 		erb :index
+
+	when "Go_on_a_Jet_Ski"
+		@title = "Weekend Vacation"
+		@story = "You enjoy yourself, but run into a dock causing severe property damage."
+
+	when "Go_Fishing_on_a_Boat"
+		@title = "Weekend Vacation"
+		@story = "You enjoy yourself, but forgot to buy a fishing license. You are jailed and forced to spend the night in prison. "
 		end
 	end
-
-get 'activity/decision/:choice' do
-	case params[:choice]
-	when "Miami/choice_1"
-	@title = "Weekend Vacation"
-
